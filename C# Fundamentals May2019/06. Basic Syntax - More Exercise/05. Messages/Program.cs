18925cc404727dc22febd65b743386e531102633
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 11:20:25 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - More Exercise/05. Messages/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = new List<char>();
+            letters.Add(' ');
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                letters.Add(i);
+            }
+
+            string sms = string.Empty;
+
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                string currentRow = Console.ReadLine();
+                int letterIndex = 0;
+                int mainDigit = int.Parse(currentRow[0].ToString());
+                if (mainDigit > 0)
+                {
+                    int offset = (mainDigit - 2) * 3;
+
+                    if (mainDigit == 8 || mainDigit == 9)
+                    {
+                        offset++;
+                    }
+                    letterIndex = offset + currentRow.Length;
+                }
+                sms += letters[letterIndex];
+            }
+            Console.WriteLine(sms);
+        }
+    }
+}
