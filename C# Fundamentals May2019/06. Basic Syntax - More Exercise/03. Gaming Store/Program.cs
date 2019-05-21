9c0ef68e86072c44e59aa1492b9728268e766edd
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 11:16:24 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - More Exercise/03. Gaming Store/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _03._Gaming_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Name Price
+            //OutFall 4                   $39.99
+            //CS: OG                      $15.99
+            //Zplinter Zell	              $19.99
+            //Honored 2                   $59.99
+            //RoverWatch                  $29.99
+            //RoverWatch Origins Edition  $39.99
+
+            double currentBalance = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            double spentMoney = 0.0;
+
+            while (input != "Game Time")
+            {
+                double gamePrice = 0.0;
+                switch (input)
+                {
+                    case "OutFall 4":
+                        gamePrice = 39.99;
+                        break;
+                    case "CS: OG":
+                        gamePrice = 15.99;
+                        break;
+                    case "Zplinter Zell":
+                        gamePrice = 19.99;
+                        break;
+                    case "Honored 2":
+                        gamePrice = 59.99;
+                        break;
+                    case "RoverWatch":
+                        gamePrice = 29.99;
+                        break;
+                    case "RoverWatch Origins Edition":
+                        gamePrice = 39.99;
+                        break;
+                    default:
+                        Console.WriteLine("Not found");
+                        input = Console.ReadLine();
+                        continue;
+                }
+                if (gamePrice <= currentBalance)
+                {
+                    currentBalance -= gamePrice;
+                    Console.WriteLine($"Bought {input}");
+                    spentMoney += gamePrice;
+                    if (currentBalance == 0)
+                    {
+                        Console.WriteLine("Out of money!");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Too Expensive");
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total spent: ${spentMoney:F2}. Remaining: ${currentBalance:F2}");
+        }
+    }
+}
