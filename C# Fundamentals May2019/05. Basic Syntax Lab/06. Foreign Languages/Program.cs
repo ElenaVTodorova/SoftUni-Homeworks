d6f0b68d35c306af6f4ae495b42b9a00d471300f
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 10:31:18 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/05. Basic Syntax Lab/06. Foreign Languages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string language = string.Empty;
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    language = "English";
+                    break;
+                case "Spain":
+                case "Mexico":
+                case "Argentina":
+                    language = "Spanish";
+                    break;
+                default:
+                    language = "unknown";
+                    break;
+            }
+            Console.WriteLine(language);
+        }
+    }
+}
