52b89a1192b09805489472837fa08f299b89cb82
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 11:14:22 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - More Exercise/02. English Name of the Last Digit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            numbers.Sort();
+            numbers.Reverse();
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
