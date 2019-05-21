1fe9e2f468ecfb7805809154c0f42b46f84dd30a
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 10:38:13 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/05. Basic Syntax Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multipier = int.Parse(Console.ReadLine());
+            int startingNumber = int.Parse(Console.ReadLine());
+            int product = 0;
+
+            if (startingNumber <=10)
+            {
+                for (int times = startingNumber; times <= 10; times++)
+                {
+                    product = multipier * times;
+                    Console.WriteLine($"{multipier} X {times} = {product}");
+                }
+            }
+            else
+            {
+                product = multipier * startingNumber;
+                Console.WriteLine($"{multipier} X {startingNumber} = {product}");
+            }
+        }
+    }
+}
