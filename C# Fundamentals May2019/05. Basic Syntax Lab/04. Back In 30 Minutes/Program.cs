aa53118242f8b9a148cfcd962336a1a974c408f4
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 10:28:52 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/05. Basic Syntax Lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            TimeSpan time = new TimeSpan(hours, minutes + 30, 0);
+            Console.WriteLine($@"{time:h\:mm}");
+        }
+    }
+}
