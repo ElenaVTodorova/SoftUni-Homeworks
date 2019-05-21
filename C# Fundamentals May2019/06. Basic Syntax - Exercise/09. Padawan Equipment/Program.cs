64e3e4e0f6c064ced634ae76adf68df2088cf588
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 11:01:44 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = double.Parse(Console.ReadLine());
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+
+            double priceForLightsabers = lightsaberPrice * Math.Ceiling(numberOfStudents * 1.1);
+            double priceForRobes = robePrice * numberOfStudents;
+            double priceForBelts = beltPrice * (numberOfStudents - numberOfStudents / 6);
+
+            double totalPrice = priceForLightsabers + priceForRobes + priceForBelts;
+
+            double diff = amount - totalPrice;
+            if (diff>=0)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {Math.Abs(diff):F2}lv more.");
+            }
+
+        }
+    }
+}
