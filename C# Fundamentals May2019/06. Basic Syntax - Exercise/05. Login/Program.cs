67cd2364c46fc1a5510808280c08f4567b4c5a68
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 10:58:08 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - Exercise/05. Login/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+            for (int i = username.Length-1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+            int counter = 0;
+            string login = Console.ReadLine();
+            while (login!= password)
+            {                
+                counter++;
+                if (counter==4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                login = Console.ReadLine();
+            }           
+
+                Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
