16e5d7afbe11b96f3ada3c5475783547f249b54c
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 10:58:47 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - Exercise/06. Strong number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int currentNumber = number;
+            int sumOfFactorials = 0;
+            while (currentNumber != 0)
+            {
+                int lastDigit = currentNumber % 10;
+                int factorial = 1;
+                for (int i = 2; i <= lastDigit; i++)
+                {
+                    factorial *= i;
+                }
+                sumOfFactorials += factorial;
+                currentNumber /= 10;
+            }
+            if (sumOfFactorials==number)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+           
+        }
+    }
+}
