d2c49e21980727b15ba65946f3912490b312f965
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-21 11:18:37 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/06. Basic Syntax - More Exercise/04. Reverse String 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Reverse_String_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                sb.Append(text[i]);
+            }            
+            Console.WriteLine(sb);
+        }
+    }
+}
