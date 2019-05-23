28a46003989d06e8d7eafcafab308f4bf59be661
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-23 13:15:39 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/07. Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double distnaceInKilometers = distanceInMeters / 1000;
+            Console.WriteLine($"{distnaceInKilometers:F2}");
+        }
+    }
+}
