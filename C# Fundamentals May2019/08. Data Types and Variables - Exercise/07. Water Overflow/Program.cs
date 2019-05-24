ed3fd145347654b6b0b607757212f5391a4c6087
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 17:06:26 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPours = int.Parse(Console.ReadLine());
+            int tankCapacity = 255;
+            int pouredLitters = 0;
+
+            for (int i = 0; i < numberOfPours; i++)
+            {
+                int litterOfWater = int.Parse(Console.ReadLine());
+                if (pouredLitters + litterOfWater <= tankCapacity)
+                {
+                    pouredLitters += litterOfWater;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(pouredLitters);
+        }
+    }
+}
