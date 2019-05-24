124693a2d5ab47b3a6e4ee55d80b1dc4b77cb39c
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 15:44:45 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCharacters = int.Parse(Console.ReadLine());
+            int sumOfChars = 0;
+            for (int i = 0; i < numberOfCharacters; i++)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                sumOfChars += currentChar;
+            }
+            Console.WriteLine($"The sum equals: {sumOfChars}");
+        }
+    }
+}
