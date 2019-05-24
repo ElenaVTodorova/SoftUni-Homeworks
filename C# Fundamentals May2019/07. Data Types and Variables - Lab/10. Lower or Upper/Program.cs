d69d161f92069a806e6437311657263506de2449
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 15:20:07 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/07. Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+            if (symbol >= 97)
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
