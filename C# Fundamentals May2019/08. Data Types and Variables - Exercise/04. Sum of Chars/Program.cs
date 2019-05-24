8c1892b1122d0c9c12beb64f699e34ae728f3094
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 15:43:58 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacityOfElevator = int.Parse(Console.ReadLine());
+            //int courses = (int)Math.Ceiling((double)numberOfPeople / capacityOfElevator);
+            int courses = numberOfPeople / capacityOfElevator;
+            if (numberOfPeople % capacityOfElevator != 0)
+            {
+                courses += 1;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
