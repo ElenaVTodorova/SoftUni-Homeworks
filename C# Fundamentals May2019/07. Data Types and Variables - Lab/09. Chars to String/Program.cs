cb11b5f8fd30514e7bdcb2c6b4910ea365d84687
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 15:17:44 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/07. Data Types and Variables - Lab/09. Chars to String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                sb.Append(Console.ReadLine());
+            }
+            string result = sb.ToString();
+            Console.WriteLine(result);
+
+            //char symbol1 = char.Parse(Console.ReadLine());
+            //char symbol2 = char.Parse(Console.ReadLine());
+            //char symbol3 = char.Parse(Console.ReadLine());
+            //string result = "" + symbol1 + symbol2 + symbol3;
+            //Console.WriteLine(result);
+        }
+    }
+}
