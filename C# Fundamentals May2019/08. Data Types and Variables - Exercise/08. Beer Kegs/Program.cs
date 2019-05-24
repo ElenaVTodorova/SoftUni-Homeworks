00fc44caea1d11fce962b34b577a1095840058c6
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 18:20:10 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfKegs = int.Parse(Console.ReadLine());
+            string biggestKeg = string.Empty;
+            double biggestVolumeOfKeg = double.MinValue;
+
+            for (int i = 0; i < numberOfKegs; i++)
+            {
+                string modelOfCurrentKeg = Console.ReadLine();
+                double radiusOfCurrentKeg = double.Parse(Console.ReadLine());
+                int heightOfCurrentKeg = int.Parse(Console.ReadLine());
+
+                double volumeOfKeg = Math.PI * radiusOfCurrentKeg * radiusOfCurrentKeg * heightOfCurrentKeg;
+
+                if (volumeOfKeg > biggestVolumeOfKeg)
+                {
+                    biggestKeg = modelOfCurrentKeg;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
