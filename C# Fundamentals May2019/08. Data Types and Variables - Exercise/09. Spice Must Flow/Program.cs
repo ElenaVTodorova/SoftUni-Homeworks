e0de6b1d60e1e81a0f2a6f262826583fa92210e8
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 18:57:05 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int spiceConsumedPerDay = 26;
+            int totalAmount = 0;
+            int days = 0;
+            int droppingYieldPerDay = 10;
+
+            while (startingYield >= 100)
+            {
+                totalAmount += startingYield - spiceConsumedPerDay;
+                startingYield -= droppingYieldPerDay;
+                days++;
+            }
+            if (totalAmount > 0)
+            {
+                totalAmount -= 26;
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
