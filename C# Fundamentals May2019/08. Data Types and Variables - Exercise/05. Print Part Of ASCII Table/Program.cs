d341fcf9085817e1eb1bf9c5d7ac545621af2a80
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 16:23:48 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstChar = int.Parse(Console.ReadLine());
+            int lastChar = int.Parse(Console.ReadLine());
+
+            for (char currentChar = (char)firstChar; currentChar <= lastChar; currentChar++)
+            {
+                Console.Write(currentChar + " ");
+            }
+        }
+    }
+}
