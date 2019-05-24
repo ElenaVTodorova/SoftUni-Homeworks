bb39727a6674a04a70faba4da2ece271835a48c9
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 15:42:12 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sumOfDigits = 0;
+
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                sumOfDigits += lastDigit;
+                number /= 10;
+            }
+            Console.WriteLine(sumOfDigits);
+        }
+    }
+}
