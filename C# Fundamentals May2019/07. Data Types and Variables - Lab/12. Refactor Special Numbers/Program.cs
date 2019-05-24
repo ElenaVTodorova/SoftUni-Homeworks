7ce5bd047c06a67969a2329a13af98ec5226b45c
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 15:21:54 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/07. Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = int.Parse(Console.ReadLine());
+            for (int currentNumber = 1; currentNumber <= lastNumber; currentNumber++)
+            {
+                int number = currentNumber;
+                int sumOfDigits = 0;
+                while (number > 0)
+                {
+                    sumOfDigits += number % 10;
+                    number /= 10;
+                }
+                bool isSpecial = sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11;
+                Console.WriteLine("{0} -> {1}", currentNumber, isSpecial);
+            }
+        }
+    }
+}
