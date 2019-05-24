2e517912931a462befe1dfac29b75b4f4099d76f
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-24 16:37:01 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLetters = int.Parse(Console.ReadLine());
+
+            for (char i = 'a'; i < 'a'+ numberOfLetters; i++)
+            {
+                for (char j = 'a'; j < 'a' + numberOfLetters; j++)
+                {
+                    for (char k = 'a'; k < 'a' + numberOfLetters; k++)
+                    {
+                        Console.WriteLine($"{i}{j}{k}");
+                    }
+                }
+            }
+        }
+    }
+}
