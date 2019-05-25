36d5fa1d653ace697a6d634d0839dd92b56bdd41
ElenaVTodorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-25 10:51:01 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/08. Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingPokePower = int.Parse(Console.ReadLine());
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int targetsCount = 0;
+            int currentPokePower = startingPokePower;
+            
+
+            while (currentPokePower >= distanceBetweenTargets)
+            {
+                currentPokePower -= distanceBetweenTargets;
+                targetsCount++;
+                bool isFifteenPercent = currentPokePower == 0.5*startingPokePower;
+                if (isFifteenPercent && exhaustionFactor != 0)
+                {
+                    currentPokePower /= exhaustionFactor;
+                }
+            }
+            Console.WriteLine(currentPokePower);
+            Console.WriteLine(targetsCount);
+        }
+    }
+}
