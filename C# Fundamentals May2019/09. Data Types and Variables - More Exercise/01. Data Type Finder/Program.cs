231a22291ae2efd6513bbc54d41dfa00df9ddaf3
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-26 13:42:20 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/09. Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string dataType = string.Empty;
+            int integer = 0;
+            double floatingPoint;
+            char character;
+            bool isBool;
+            while (input != "END")
+            {
+                if (int.TryParse(input, out integer))
+                {
+                    dataType = "integer";
+                }
+                else if (double.TryParse(input, out floatingPoint))
+                {
+                    dataType = "floating point";
+                }
+                else if (char.TryParse(input, out character))
+                {
+                    dataType = "character";
+                }
+                else if (bool.TryParse(input, out isBool))
+                {
+                    dataType = "boolean";
+                }
+                else
+                {
+                    dataType = "string";
+                }
+                Console.WriteLine($"{input} is {dataType} type");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
