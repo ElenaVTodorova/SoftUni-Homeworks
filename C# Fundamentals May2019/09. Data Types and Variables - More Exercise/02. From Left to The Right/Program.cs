027dbaab26c7ea4a096210ef6ad2e5bdf855a005
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-26 14:50:05 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/09. Data Types and Variables - More Exercise/02. From Left to The Right/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._From_Left_to_The_Right
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int currentLine = 0; currentLine < numberOfLines; currentLine++)
+            {
+                long[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(long.Parse)
+                    .ToArray();
+                long maxNumber = Math.Max(numbers[0], numbers[1]);
+                maxNumber = Math.Abs(maxNumber);
+                long sumOfDigits = 0;
+                while (maxNumber > 0)
+                {
+                    sumOfDigits += maxNumber % 10;
+                    maxNumber /= 10;
+                }
+                Console.WriteLine(sumOfDigits);
+            }
+        }
+    }
+}
