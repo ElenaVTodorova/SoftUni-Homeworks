b31c2696d503a8fb26ba15223eba932ca48434f8
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-28 11:07:09 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/09. Data Types and Variables - More Exercise/04. Refactoring: Prime Checker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04._Refactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = int.Parse(Console.ReadLine());
+            for (int currentNumber = 2; currentNumber <= lastNumber; currentNumber++)
+            {
+                bool isPrime = true;
+                for (int i = 2; i < currentNumber; i++)
+                {
+                    if (currentNumber % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine($"{currentNumber} -> true");
+                }
+                else
+                {
+                    Console.WriteLine($"{currentNumber} -> false");
+                }
+                
+            }
+
+        }
+    }
+}
