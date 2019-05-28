9feda9dc33401faf1ce855cce0224c877887601e
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-28 11:23:04 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/09. Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberOfLines = int.Parse(Console.ReadLine());
+            string decryptedMessage = string.Empty;
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                char character = char.Parse(Console.ReadLine());
+                char decryptedChar = (char)(character + key);
+                decryptedMessage += decryptedChar;
+            }
+            
+            Console.WriteLine(decryptedMessage);
+        }
+    }
+}
