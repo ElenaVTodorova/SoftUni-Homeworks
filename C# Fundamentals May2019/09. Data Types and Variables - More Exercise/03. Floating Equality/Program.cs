501b1bf6eb20fb0cbe472899a77dab54cee7029f
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-28 11:06:02 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/09. Data Types and Variables - More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double diff = Math.Abs(firstNumber - secondNumber);
+            double eps = 0.000001;
+
+            Console.WriteLine(diff<eps);
+        }
+    }
+}
