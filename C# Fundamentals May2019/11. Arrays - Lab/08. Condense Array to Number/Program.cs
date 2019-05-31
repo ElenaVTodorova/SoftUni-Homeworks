752a98d04281e1370b9aec1eb88e3026c60150ba
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-31 18:03:28 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/11. Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            
+            int[] condensed = new int[numbers.Length - 1];
+            
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = 0; j < condensed.Length; j++)
+                {
+                    condensed[j] = numbers[j] + numbers[j + 1];
+                    numbers[j] = condensed[j];
+                }
+            }
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
