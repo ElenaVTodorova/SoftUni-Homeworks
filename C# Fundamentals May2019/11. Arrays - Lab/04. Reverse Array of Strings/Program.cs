e2639036b5aceb39cfae86b7d69928f643bd891c
Elena Todorova <50674854+ElenaVTodorova@users.noreply.github.com>
2019-05-31 17:58:49 +0300

    Add files via upload

---
+++ C# Fundamentals May2019/11. Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfStrings = Console.ReadLine().Split();
+
+            for (int i = 0; i < arrayOfStrings.Length / 2; i++)
+            {
+                string oldElement = arrayOfStrings[i];
+                arrayOfStrings[i] = arrayOfStrings[arrayOfStrings.Length - 1 - i];
+                arrayOfStrings[arrayOfStrings.Length - 1 - i] = oldElement;
+            }
+
+            Console.WriteLine(string.Join(" ", arrayOfStrings));
+        }
+    }
+}
